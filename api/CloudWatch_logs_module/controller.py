a1df528cc32f6ac3faf8198e4d17759512a30c88
Tushar Ahire <tushar.ahire@opcito.com>
2023-01-19 13:23:20 +0530

    updates get_log_groups method

--- api/CloudWatch_logs_module/controller.py
+++ api/CloudWatch_logs_module/controller.py
@@ -12,25 +12,30 @@ from .service import convert_to_miliseconds
 def get_log_groups():
 
     try:
+        request_obj=request.json
         # creating a client
         current_app.logger.info("Creating client")
         client = boto3.client('logs', aws_access_key_id=aws_access_key_id,
                               aws_secret_access_key=aws_secret_access_key,
-                              region_name=region_name)
+                              region_name=request_obj["region_name"])
         
         # describing the log groups
         current_app.logger.info("Describing the log groups")
         response = client.describe_log_groups(
-            logGroupNamePrefix=f'/aws/rds/instance/{request.json.get("db_name")}',
+            logGroupNamePrefix=f'/aws/rds/instance/{request_obj["db_name"]}',
         )
 
         # sending response
         current_app.logger.info("Sending Response")
-        return jsonify(response), HTTPStatus.OK
-
+        return jsonify(response["logGroups"]), HTTPStatus.OK
+    
+    except KeyError as e:
+        current_app.logger.error(str(e))
+        return {"Error":str(e)},HTTPStatus.BAD_REQUEST
+    
     except Exception as e:
         current_app.logger.error(str(e))
-        return {"Error": str(e)}, HTTPStatus.BAD_REQUEST
+        return {"Error":str(e)}, HTTPStatus.BAD_REQUEST
 
 
 def get_log_streams():
@@ -40,18 +45,11 @@ def get_log_streams():
                               aws_secret_access_key=aws_secret_access_key,
                               region_name=region_name)
 
-        start_time = request.json.get('start_time')
-        end_time = request.json.get('end_time')
-        start_time_miliseconds = convert_to_miliseconds(start_time)
-        end_time_miliseconds = convert_to_miliseconds(end_time)
+      
         
-        response = client.filter_log_events(
-            logGroupName='/aws/rds/instance/database-1/slowquery',
-            startTime=start_time_miliseconds,
-            endTime=end_time_miliseconds,
-        )
-
-        return jsonify(response)
+            
+       
+        return 
 
     except Exception as e:
         return {"Error": str(e)}
--- api/CloudWatch_logs_module/service.py
+++ api/CloudWatch_logs_module/service.py
@@ -8,13 +8,6 @@ def convert_to_miliseconds(Time):
     return time_in_miliseconds
     
 
-# start_time = datetime.strptime(
-#     '17/1/2023 11:13:08.230010', '%d/%m/%Y %H:%M:%S.%f')
-# end_time = datetime.strptime(
-#     '17/1/2023 11:13:10.230010', '%d/%m/%Y %H:%M:%S.%f')
-# diff = end_time-start_time
-# mill = diff.total_seconds() * 1000
 
-# print("Seconds: ", mill)
 
 
